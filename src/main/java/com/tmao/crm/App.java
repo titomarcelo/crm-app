58e858c3cca50ff449b4d41712d97134b1ddbf85
Tito Marcelo <tmoliveira@gazeus.com>
2020-09-23 18:37:27 -0300

    test

--- src/main/java/com/tmao/crm/App.java
+++ src/main/java/com/tmao/crm/App.java
@@ -1,19 +1,10 @@
 package com.tmao.crm;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class App extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(App.class);
-    }
+@SpringBootApplication(scanBasePackages = {"com.tmao.crm"})
+public class App {
 
     public static void main(final String[] args) {
         SpringApplication.run(App.class, args);
